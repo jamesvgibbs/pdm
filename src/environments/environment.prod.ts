f9ac8e2b5651726ab5053db3fd6f79ec8fd350a8
Jim Gibbs <jim@streamlinecoach.com>
2017-08-21 18:19:05 -0400

    initial load

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDxoEml5aCjy_Gjp0HY0ouOOslqH-vDsEo',
+    authDomain: 'nmsi-8118d.firebaseapp.com',
+    databaseURL: 'https://nmsi-8118d.firebaseio.com',
+    projectId: 'nmsi-8118d',
+    storageBucket: 'nmsi-8118d.appspot.com',
+    messagingSenderId: '174952877450'
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDxoEml5aCjy_Gjp0HY0ouOOslqH-vDsEo',
+    authDomain: 'nmsi-8118d.firebaseapp.com',
+    databaseURL: 'https://nmsi-8118d.firebaseio.com',
+    projectId: 'nmsi-8118d',
+    storageBucket: 'nmsi-8118d.appspot.com',
+    messagingSenderId: '174952877450'
+  }
+};
